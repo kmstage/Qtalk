897e4c659184a6ae856f329e4e91aecdca4abdb1
kave moradian <kaveh@localhost.localdomain>
2020-06-01 23:41:02 +0430

    database created

--- Qtalk/main.py
+++ Qtalk/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '50bec9ca1cabe7bee53088c068b7524f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 posts = [
     {'username': 'Kaveh.m',
