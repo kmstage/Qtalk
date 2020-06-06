96d453cfe0c96ef23ce00321eaa64247d4ed2345
kave moradian <kaveh@localhost.localdomain>
2020-06-06 21:19:45 +0430

    create modal for edit btn

--- Qtalk/routes.py
+++ Qtalk/routes.py
@@ -8,16 +8,17 @@ from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
 
 
-
 @app.route('/')
 @app.route('/home')
 def home():
+    form = UpdateForm()
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=25)
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, form=form)
 
 @app.route('/about')
 def about():
+
     return render_template('about.html')
 
 @app.route('/register', methods=['GET', 'POST'])
